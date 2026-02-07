75ff5bafe7f6f91cb2b882580711fef080cdf0a0
ingram34bro-debug <ingram34bro@gmail.com>
2026-02-07 10:25:36 +0800

    dfs

---
+++ cs336_basics/predict.py
@@ -0,0 +1,170 @@
+from typing import Optional, Iterable, Iterator
+
+import time
+import regex as re
+import os
+import tqdm
+import multiprocessing as mp
+import numpy as np
+
+import torch
+
+from cs336_basics.train_together import load_meta, create_model, MyLoadCheckpoint
+from cs336_basics.tokenizer_fast import Tokenizer
+
+def load_model(
+    meta_path: str,
+    checkpoint_path: str,
+    device: str = torch.cuda.current_device() if torch.cuda.is_available() else "cpu",
+):
+    meta = load_meta(meta_path)
+    model = create_model(meta, device)
+    _, _ = MyLoadCheckpoint(checkpoint_path, model, device=device)
+    return model, meta
+
+def get_Tokenizer(vocab_path: str, merges_path: str, special_tokens: Optional[list[bytes]] = None) -> Tokenizer:
+    tokenizer = Tokenizer.from_files(vocab_path, merges_path, special_tokens=special_tokens)
+    return tokenizer
+
+class MyNaiveGPT:
+    def __init__(
+        self,
+        model: torch.nn.Module,
+        tokenizer: Tokenizer,
+        device: str = torch.cuda.current_device() if torch.cuda.is_available() else "cpu",
+        seed: Optional[int] = None,
+    ):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.device = device
+        
+        if seed is None:
+            seed = int(time.time())
+        
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+        self._seed = seed
+        
+        self._eof = tokenizer.encode("<|endoftext|>")[0]
+        
+        self.max_content_length = self.model.max_seq_len
+    
+    @classmethod
+    def from_paths(
+        cls,
+        meta_path: str,
+        checkpoint_path: str,
+        vocab_path: str,
+        merges_path: str,
+        special_tokens: Optional[list[bytes]] = None,
+        device: str = torch.cuda.current_device() if torch.cuda.is_available() else "cpu",
+    ):
+        model, _ = load_model(meta_path, checkpoint_path, device)
+        tokenizer = get_Tokenizer(vocab_path, merges_path, special_tokens=special_tokens)
+        return cls(model, tokenizer, device)
+    
+    def _get_logits(self, input_ids: torch.Tensor) -> torch.Tensor:
+        self.model.eval()
+        with torch.no_grad():
+            logits = self.model(input_ids)
+        return logits
+    
+    def _softmax_with_temperature(self, logits: torch.Tensor, temperature: float = 1.0) -> torch.Tensor:
+        if temperature < 0:
+            raise ValueError("Temperature must be positive.")
+        elif temperature == 0:
+            logits_max = torch.max(logits, dim=-1, keepdim=True).values
+            probs = torch.zeros_like(logits)
+            probs[logits == logits_max] = 1.0
+            probs /= torch.sum(probs, dim=-1, keepdim=True)
+        else:
+            scaled_logits = logits / temperature
+            exp_logits = torch.exp(scaled_logits - torch.max(scaled_logits, dim=-1, keepdim=True).values)
+            probs = exp_logits / torch.sum(exp_logits, dim=-1, keepdim=True)
+        return probs
+    
+    def _top_p_sample(self, probs, top_p=0.9):
+        if not (0.0 < top_p <= 1.0):
+            raise ValueError("top_p must be in (0, 1]")
+
+        sorted_probs, sorted_indices = torch.sort(probs, descending=True)
+        cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+
+        if cumulative_probs[-1] < top_p:
+            cutoff = cumulative_probs.numel() - 1
+        else:
+            cutoff = torch.searchsorted(cumulative_probs, top_p)
+
+        filtered_probs = sorted_probs[:cutoff + 1]
+        filtered_indices = sorted_indices[:cutoff + 1]
+
+        filtered_probs = filtered_probs / filtered_probs.sum()
+
+        next_idx = torch.multinomial(filtered_probs, 1)
+        return filtered_indices[next_idx].item()
+    
+    def _next_token(
+        self,
+        input_ids: torch.Tensor,
+        temperature: float = 1.0,
+        top_p: float = 0.9,
+    ) -> int:
+        logits = self._get_logits(input_ids)
+        last_logits = logits[:, -1, :]
+        probs = self._softmax_with_temperature(last_logits, temperature=temperature).squeeze(0)
+        next_token = self._top_p_sample(probs, top_p=top_p)
+        return next_token
+    
+    def generate(
+        self,
+        prompt: str,
+        max_new_tokens: int = 50,
+        temperature: float = 1.0,
+        top_p: float = 0.9,
+    ) -> str:
+        input_ids = self.tokenizer.encode(prompt)
+        input_tensor = torch.tensor([input_ids], dtype=torch.long).to(self.device)
+        
+        generated_ids = input_ids.copy()
+        
+        progress = tqdm.trange(max_new_tokens, desc="Generating", unit="token")
+        for _ in progress:
+            next_token = self._next_token(input_tensor, temperature=temperature, top_p=top_p)
+            if next_token == self._eof:
+                progress.write("End-of-Text token generated. Stopping generation.")
+                break
+            generated_ids.append(next_token)
+            
+            if len(generated_ids) > self.max_content_length:
+                input_tensor = torch.tensor([generated_ids[-self.max_content_length:]], dtype=torch.long).to(self.device)
+            else:
+                input_tensor = torch.tensor([generated_ids], dtype=torch.long).to(self.device)
+        
+        generated_text = self.tokenizer.decode(generated_ids)
+        return generated_text
+
+if __name__ == "__main__":
+    meta_path = "/home/std7/extend/llm-from-scratch-assignment1-basics/my_model/meta.pkl"
+    model_path = "/home/std7/extend/lfs-data/checkpoint.pth"
+    vocab_path = "/home/std7/extend/lfs-data/owt_bpe_vocab.pkl"
+    merge_path = "/home/std7/extend/lfs-data/owt_bpe_merges.pkl"
+    
+    special_tokens = [b"<|endoftext|>"]
+    
+    my_gpt = MyNaiveGPT.from_paths(
+        meta_path=meta_path,
+        checkpoint_path=model_path,
+        vocab_path=vocab_path,
+        merges_path=merge_path,
+        special_tokens=special_tokens,
+        device="cuda" if torch.cuda.is_available() else "cpu",
+    )
+    
+    test_prompt = "I love you."
+    generated_text = my_gpt.generate(
+        prompt=test_prompt,
+        max_new_tokens=1000,
+        temperature=0.8,
+        top_p=0.9,
+    )
+    print(generated_text)
--- cs336_basics/train_together.py
+++ cs336_basics/train_together.py
@@ -1,102 +1,151 @@
-from __future__ import annotations
-
-import os
-import argparse
-import numpy as np
 import torch
-import torch.nn.functional as F
-import json
-import tqdm
-from collections.abc import Iterable
-from typing import IO, Any, BinaryIO
-
-import numpy.typing as npt
-import torch
-from jaxtyping import Bool, Float, Int
 from torch import Tensor
-from cs336_basics.bpe import *
-from cs336_basics.bpe_tokenizer import *
-from cs336_basics.layer import *
-from cs336_basics.TransformerBlock import *
-from cs336_basics.TransformerLM import *
-from cs336_basics.cross_entropy_loss import *
-from cs336_basics.optimizer import *
-from cs336_basics.clip_gradient import *
-from cs336_basics.get_batch import *
-from cs336_basics.check_and_load import *
+import numpy as np
 
-#增加存储loss的部分
-def new_save_checkpoint(model, optimizer, iteration, loss,out) -> None:
+from typing import Optional
+from jaxtyping import Bool, Float, Int
+import os
+import typing
+import numpy.typing as npt
+
+from collections.abc import Callable, Iterable
+import math
+
+import tqdm
+import time
+import pickle
+
+
+from einops import rearrange, einsum
+
+def create_model(meta: dict, device: str) -> torch.nn.Module:
+    model = TransformerLM(
+        num_embeddings = meta["vocab_size"],
+        d_model = meta["d_model"],
+        num_layers = meta["num_layers"],
+        num_heads = meta["num_heads"],
+        theta= meta["theta"],
+        max_seq_len= meta["max_seq_len"],
+        d_ff = meta["d_ff"],
+        device=device,
+    ).to(device)
+    return model
+
+def load_meta(meta_path: str) -> dict:
+    with open(meta_path, "rb") as f:
+        meta = pickle.load(f)
+    return meta
+
+def MyGetBatch(dataset: npt.NDArray, batch_size: int, context_length: int, device: str
+) -> tuple[torch.Tensor, torch.Tensor]:
+    data_len = dataset.shape[0]
+    ix = np.random.randint(0, data_len - context_length, size=batch_size)
+
+    x = np.empty((batch_size, context_length), dtype=np.int64)
+    y = np.empty_like(x)
+
+    for k, i in enumerate(ix):
+        x[k] = dataset[i : i + context_length]
+        y[k] = dataset[i + 1 : i + context_length + 1]
+
+    return (
+        torch.from_numpy(x).to(device),
+        torch.from_numpy(y).to(device),
+    )
+
+def MySaveCheckpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    train_log: dict,
+) -> None:
     checkpoint = {
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'iteration': iteration,
-        "loss": loss if loss is not None else [],
-
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "iteration": iteration,
+        "train_log": train_log,
     }
     torch.save(checkpoint, out)
 
-#增加读取loss的部分
-def new_load_checkpoint(src,model, optimizer) ->  tuple[int, Any]:
-    checkpoint = torch.load(src)
-    checkpoint = torch.load(src)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    iteration = checkpoint['iteration']
-    loss=checkpoint.get("loss", [])
-    return iteration,loss
 
-def SingleTrain(model: torch.nn.Module, optimizer: torch.optim.Optimizer, 
-            data: tuple[Tensor, Tensor], T: int, lrarg: tuple[float, float, int, int]) -> float:
+def MyLoadCheckpoint(
+    src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    model: torch.nn.Module,
+    optimizer: Optional[torch.optim.Optimizer] = None,
+    device: Optional[str] = None,
+):
+    checkpoint = torch.load(src, map_location=device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    if optimizer is not None:
+        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    iteration = checkpoint["iteration"]
+    train_log = checkpoint.get("train_log", None)
+    return iteration, train_log
+
+# 定义单次训练：
+
+from optimizer import *
+from get_batch import *
+from TransformerLM import TransformerLM
+
+def SingleTrain(model: torch.nn.Module, optimizer: torch.optim.Optimizer, data: tuple[Tensor, Tensor], T: int, lrarg: tuple[float, float, int, int]) -> float:
     lr = get_lr_cosine(T, *lrarg)
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
+    
     model.train()
-    x, y = data
-    logits = model(x)
-    logits=rearrange(logits,'b c v -> (b c) v')
-    targets = rearrange(y,'b c -> (b c)')
-    #loss = cross_entropy_loss(logits,targets)
+    # x_batch, y_batch = MyGetBatch(dataset, batch_size, context_length, device)
+    x_batch, y_batch = data  # (batch_size, context_length), (batch_size, context_length)
+    
+    logits = model(x_batch)  # (batch_size, context_length, vocab_size)
+    logits = rearrange(logits, "b c v -> (b c) v")  # (batch_size * context_length, vocab_size)
+    targets = rearrange(y_batch, "b c -> (b c)")  # (batch_size * context_length,)
+
+    # ========= 只改这里 =========
     loss = torch.nn.functional.cross_entropy(logits, targets)
+    # ===========================
+
     optimizer.zero_grad()
     loss.backward()
     
-    #gradient_clipping(model.parameters(), max_norm)
+    # MyGradientClipping(model.parameters(), max_norm=1.0)
     torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+    
     optimizer.step()
+    
     return loss.item()
 
-@torch.no_grad() # 关键：禁用梯度计算，节省显存和计算量
-def estimate_loss(model, dataset, batch_size, context_length, device, eval_iters=10):
-    model.eval() # 切换到评估模式（关闭 Dropout 等）
-    losses = []
-    for _ in range(eval_iters):
-        # 获取验证集随机 batch
-        x, y = get_batch(dataset, batch_size, context_length, device)
-        logits = model(x)
-        
-        # 维度变换以匹配 CrossEntropy
-        logits = rearrange(logits, 'b c v -> (b c) v')
-        targets = rearrange(y, 'b c -> (b c)')
-        
+def SingleValidate(model: torch.nn.Module, dataset: npt.NDArray, batch_size: int, context_length: int, device: str) -> float:
+    model.eval()
+    with torch.no_grad():
+        data = MyGetBatch(dataset, batch_size, context_length, device)
+        x_batch, y_batch = data
+        logits = model(x_batch)
+        logits = rearrange(logits, "b c v -> (b c) v")
+        targets = rearrange(y_batch, "b c -> (b c)")
         loss = torch.nn.functional.cross_entropy(logits, targets)
-        losses.append(loss.item())
-    
-    model.train() # 切换回训练模式
-    return np.mean(losses)
+    return loss.item()
+
 
 def _innerTrain(
     model: torch.nn.Module,
     optimizer: torch.optim.Optimizer,
     dataset: npt.NDArray,
-    dataset_valid:npt.NDArray,
     batch_size: int,
     context_length: int,
     device: str,
     start_step: int,
     lrarg: tuple[float, float, int, int],
     max_iters: int,
-) -> float:
+    val_dataset: Optional[npt.NDArray] = None,
+    train_log: Optional[dict] = None,
+):
+    if train_log is None:
+        train_log = {
+            "train": [],
+            "valid": [],
+        }
     
     # 进度条，从T进度回复
     progress = tqdm.tqdm(
@@ -107,85 +156,169 @@ def _innerTrain(
         unit="iter",
     )
     
-    losses = []
-    val_losses=[]
+    start_time = time.time()
+    target_time = 7 * 60 * 60  # 7 hours
+    
+    ex = None
     try:
         for T in progress:
-            data = get_batch(dataset, batch_size, context_length, device)
+            data = MyGetBatch(dataset, batch_size, context_length, device)
             loss = SingleTrain(model, optimizer, data, T, lrarg)
-            losses.append(loss)
-            if T % 100 == 0: 
-                print(np.mean(losses[T//100-1:T//100]))
-                val_loss = estimate_loss(model, dataset_valid, batch_size, context_length, device)
-                print(f"Step {T}: Train Loss {loss:.4f}, Val Loss {val_loss:.4f}")
-                val_losses.append((T, val_loss)) # 记录步数和对应的验证损失
+            elapsed = time.time() - start_time
+
+            train_log["train"].append({
+                "step": T,
+                "loss": loss,
+                "time": elapsed,
+            })
+            
+            if val_dataset is not None and (T + 1) % 100 == 0:
+                val_loss = SingleValidate(model, val_dataset, batch_size, context_length, device)
+                train_log["valid"].append({
+                    "step": T,
+                    "loss": val_loss,
+                    "time": elapsed,
+                })
+                progress.set_postfix({"loss": loss, "val_loss": val_loss})
+            
+            if time.time() - start_time > target_time:
+                print(f"Reached target training time of {target_time} seconds at step {T}.")
+                break
+        
         success = True
     except BaseException as e:
-        progress.close()
+        ex = e
         print(f"Training interrupted at step {T} due to exception: {e}")
         success = False
+    finally:
+        progress.close()
     
-    return success, T, losses,val_loss
+    return success, T, train_log, ex
 
-def train(path: str,dataset_path: str,vocab_size:int=10000,device: str="cpu",dtype=None,
-          batch_size: int=32,context_length: int=256,
-          T: int=1000,lrarg: tuple[float, float, int, int]=None):
+def Train(
+    path: str,
+    dataset_path: str,
+    meta_path: str,
+    val_dataset_path: Optional[str] = None,
+    # 初创时需要的参数
+    **meta_kwargs,
+):
+    # 读取数据集
     dataset = np.memmap(
         dataset_path,
         dtype=np.uint16,
         mode="r",
     )
-    factory_kwargs = {"device": device, "dtype": torch.bfloat16}
-    model = TransformerLM(
-        vocab_size=vocab_size,context_length=context_length,
-        d_model=512,
-        num_layers=4,
-        num_heads=32,
-        d_ff=1344,
-        rope_theta=10000,
-        **factory_kwargs
-    )
-    model.to(device)
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=1e-2)
-    start_step = 0
-    if lrarg is None:
-        lrarg =[1e-3,1e-5,1000,10000] #如果没有参数，设置一个lr参数
+    
+    if val_dataset_path is not None:
+        val_dataset = np.memmap(
+            val_dataset_path,
+            dtype=np.uint16,
+            mode="r",
+        )
+    else:
+        val_dataset = None
+    
+    # 读取元信息
+    if os.path.exists(meta_path):
+        with open(meta_path, "rb") as f:
+            meta = pickle.load(f)
+    else:
+        meta = {}
+        meta.update(meta_kwargs)
+        with open(meta_path, "wb") as f:
+            pickle.dump(meta, f)
+    
+    if "device" in meta_kwargs:
+        meta["device"] = meta_kwargs["device"]
+    
+    print(meta)
 
+    device = meta.get("device", "cpu")
+    
+    model = create_model(meta, device)
+    
+    # 尝试加载检查点
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3, weight_decay=0.01)
+    start_step = 0
     if os.path.exists(path):
-        start_step, prev_losses = new_load_checkpoint(path, model, optimizer)
+        start_step, train_log = MyLoadCheckpoint(path, model, optimizer, device)
+        if train_log is None:
+            train_log = {
+                "meta": meta,
+                "train": [],
+                "valid": [],
+            }
         print(f"Resumed from checkpoint at step {start_step}.")
     else:
-        prev_losses = []
+        train_log = {
+            "meta": meta,
+            "train": [],
+            "valid": [],
+        }
     
-    success, final_step, losses = _innerTrain(
+    success, final_step, train_log, e = _innerTrain(
         model=model,
         optimizer=optimizer,
         dataset=dataset,
-        dataset_valid=np.memmap(
-            '/home/std7/extend/lfs-data/TinyStoriesV2-GPT4-valid.npy',
-            dtype=np.uint16,
-            mode="r",
-        ),
-        batch_size=batch_size,
-        context_length=context_length,
+        batch_size=meta["batch_size"],
+        context_length=meta["max_seq_len"],
         device=device,
         start_step=start_step,
-        lrarg=lrarg,
-        max_iters=T,
+        lrarg=(
+            meta["lr_initial"],
+            meta["lr_final"],
+            meta["lr_warmup_iters"],
+            meta["max_iters"],
+        ),
+        max_iters=meta["max_iters"],
+        val_dataset=val_dataset,
     )
     
-    new_save_checkpoint(model, optimizer, final_step, loss=prev_losses+losses,out=path,)
+    MySaveCheckpoint(
+        model=model,
+        optimizer=optimizer,
+        iteration=final_step,
+        out=path,
+        train_log=train_log,
+    )
+    
+    return not isinstance(e, KeyboardInterrupt)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     
+    import cProfile
+    import pstats
     
-    train(
-        path='checkpoint',
-        dataset_path='/home/std7/extend/lfs-data/TinyStoriesV2-GPT4-train.npy',
-        vocab_size=10000,
-        dtype=torch.bfloat16,
-        batch_size=32,
-        context_length=256,
-        T=10000,
+    pr = cProfile.Profile()
+    pr.enable()
+
+    if_shutdown = Train(
+        path="/home/std7/extend/llm-from-scratch-assignment1-basics/my_module/checkpoint.pth",
+        dataset_path="/home/std7/extend//lfs-data/owt_valid.npy",
+        meta_path="/home/std7/extend/llm-from-scratch-assignment1-basics/my_module/meta.pkl",
+        # 结构参数
+        vocab_size=32000,
+        num_layers=4,
+        d_model=16,
+        num_heads=4,
+        d_ff=16,
+        # 网络超参数
+        theta=0.7,
+        max_seq_len=128,
+        # 训练超参数
+        batch_size=16,
+        lr_initial=1e-3,
+        lr_final=1e-5,
+        lr_warmup_iters=1000,
+        max_iters=10000,
         device="cuda" if torch.cuda.is_available() else "cpu",
     )
+
+    pr.disable()
+
+    stats = pstats.Stats(pr)
+    stats.strip_dirs()
+    stats.sort_stats("cumtime")
+    stats.print_stats(30)
+    
